db556582dcc6c9123899e8af8658984462e54bc5
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-05-30 07:16:12 +0600

    single room details get api created

--- index.js
+++ index.js
@@ -84,6 +84,14 @@ async function run() {
       res.send(rooms);
     });
 
+    // get single room data from db using id
+    app.get("/room/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await roomsCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
