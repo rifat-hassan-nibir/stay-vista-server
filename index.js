8afb4242639510e56103cc47b981fec384148451
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-06-04 09:54:31 +0600

    user role update api created

--- index.js
+++ index.js
@@ -127,6 +127,18 @@ async function run() {
       res.send(result);
     });
 
+    // update a user role
+    app.patch("/users/update/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const query = { email: email };
+      const updateDoc = {
+        $set: { ...user, timestamp: Date.now() },
+      };
+      const result = await usersCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
     // Get rooms data from db
     app.get("/rooms", async (req, res) => {
       const category = req.query.category;
